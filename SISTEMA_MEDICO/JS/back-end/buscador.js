529e9ad3890ea17fbe81605017213b724054c951
luisdc12 <luispedro1200@gmail.com>
2023-05-16 11:50:33 -0600

    Se agrego la funcion de registrar pacientes

--- SISTEMA_MEDICO/JS/back-end/buscador.js
+++ SISTEMA_MEDICO/JS/back-end/buscador.js
@@ -1,4 +1,4 @@
-function search() {
+/*function search() {
     // Obtener los datos ingresados por el usuario
     var input1 = document.getElementById("input1").value.toUpperCase();
     var input2 = document.getElementById("input2").value.toUpperCase();
@@ -46,4 +46,152 @@ function search() {
         }
       }
     }
+  }*/
+
+ /* let FormularioPacientes;
+  let Pacientes=[]
+
+  class Paciente{
+    constructor(NombreBC, ApBC, AmBC, SexoBC, TsBC, EdadBC, PesoBC, EstaturaBC, CURPBC){
+      this.NombreBC= NombreBC;
+      this.ApBC= ApBC;
+      this.AmBC= AmBC;
+      this.SexoBC=SexoBC;
+      this.TsBC=TsBC;
+      this.EdadBC=EdadBC;
+      this.PesoBC=PesoBC;
+      this.EstaturaBC=EstaturaBC;
+      this.CURPBC=CURPBC;
+    }
   }
+
+ FormularioPacientes = document.getElementById("Formulario-Pacientes");
+ FormularioPacientes.addEventListener("submit", registrarPaciente);
+
+ function registrarPaciente(event) {
+  event.preventDefault(); //EVITA QUE SE RECARGUE LA PAGINA AL ENVIAR EL FORMULARIO
+
+  //OBTENER LOS VALORES DEL FORMULARIO
+  const NombreBC = document.getElementById("NombreBC").value;
+  const ApBC = document.getElementById("ApBC").value;
+  const AmBC = document.getElementById("AmBC").value;
+  const SexoBC = document.getElementById("SexoBC").value;
+  const TsBC = document.getElementById("TsBC").value;
+  const EdadBC = document.getElementById("EdadBC").value;
+  const PesoBC= document.getElementById("PesoBC").value;
+  const EstaturaBC= document.getElementById("EstaturaBC").value;
+  const CURPBC= document.getElementById("CURPBC").value;
+
+
+  //Crea una nueva instacia de la clase cita
+  const nuevoPaciente = new Paciente(NombreBC, ApBC, AmBC, SexoBC, TsBC, EdadBC, PesoBC, EstaturaBC, CURPBC);
+
+  //Se agrega la cita al array citas
+  citas.push(nuevoPaciente);
+
+  
+
+  //LLAMAR A LA FUNCION PARA MOSTRAR LAS CITAS DEL DIA
+  mostrarPacientesBC();
+
+  //!-------------------- */
+  //!-------------------- */
+  //! */ Aquí puedes realizar cualquier acción adicional, como enviar la cita al servidor, almacenarla en una base de datos, etc.
+  //!-------------------- */
+  //!-------------------- */
+
+  //SOLO PARA VER QUE SE ESTA GURDANDO ELIMINAR DESPUES
+ // console.log("Paciente Registrado", nuevoPaciente);
+  //LIMPIA EL FORMULARIO DESPUES DE GUARDAR
+ // FormularioPacientes.reset();
+
+  
+ // Definición de la clase Paciente
+ let Pacientes = [];
+
+class Paciente {
+  constructor(NombreBC, ApBC, AmBC, SexoBC, TsBC, EdadBC, PesoBC, EstaturaBC, CURPBC) {
+    this.NombreBC = NombreBC;
+    this.ApBC = ApBC;
+    this.AmBC = AmBC;
+    this.SexoBC = SexoBC;
+    this.TsBC = TsBC;
+    this.EdadBC = EdadBC;
+    this.PesoBC = PesoBC;
+    this.EstaturaBC = EstaturaBC;
+    this.CURPBC = CURPBC;
+  }
+}
+
+function registrar() {
+  const nombre = document.getElementById("NombreBC").value;
+  const apellidoPaterno = document.getElementById("ApBC").value;
+  const apellidoMaterno = document.getElementById("AmBC").value;
+  const genero = document.getElementById("SexoBC").value;
+  const tipoSangre = document.getElementById("TsBC").value;
+  const edad = document.getElementById("EdadBC").value;
+  const peso = document.getElementById("PesoBC").value;
+  const estatura = document.getElementById("EstaturaBC").value;
+  const curp = document.getElementById("CURPBC").value;
+
+  const nuevoPaciente = new Paciente(
+    nombre,
+    apellidoPaterno,
+    apellidoMaterno,
+    genero,
+    tipoSangre,
+    edad,
+    peso,
+    estatura,
+    curp
+  );
+
+  Pacientes.push(nuevoPaciente);
+
+  mostrarPacientes();
+  limpiarFormulario();
+}
+
+function mostrarPacientes() {
+  const tablaPacientes = document.getElementById("tabla-pacientes");
+  const tbody = tablaPacientes.querySelector("tbody");
+  tbody.innerHTML = "";
+
+  Pacientes.forEach((paciente) => {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+      <td>${paciente.NombreBC}</td>
+      <td>${paciente.ApBC}</td>
+      <td>${paciente.AmBC}</td>
+      <td>${paciente.SexoBC}</td>
+      <td>${paciente.TsBC}</td>
+      <td>${paciente.EdadBC}</td>
+      <td>${paciente.PesoBC}</td>
+      <td>${paciente.EstaturaBC}</td>
+      <td>${paciente.CURPBC}</td>
+    `;
+    tbody.appendChild(fila);
+  });
+}
+
+function limpiarFormulario() {
+  document.getElementById("NombreBC").value = "";
+  document.getElementById("ApBC").value = "";
+  document.getElementById("AmBC").value = "";
+  document.getElementById("SexoBC").selectedIndex = 0;
+  document.getElementById("TsBC").selectedIndex = 0;
+  document.getElementById("EdadBC").value = "";
+  document.getElementById("PesoBC").value = "";
+  document.getElementById("EstaturaBC").value = "";
+  document.getElementById("CURPBC").value = "";
+}
+
+function confirmarRegistro() {
+  const confirmado = window.confirm("¿Deseas registrar los datos del paciente?");
+  if (confirmado) {
+    registrar();
+  }
+}
+
+// Llamada inicial para mostrar los pacientes si existen
+mostrarPacientes();
